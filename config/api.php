8107b9147219fc90bdec9264b36443c156510519
George Lemon <things@wemake.co.de>
2020-12-29 20:11:54 +0200

    Add api configuration
    Signed-off-by: George Lemon <things@wemake.co.de>

---
+++ config/api.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /**
+     * The base URI of the basic API
+     */
+    'base' => 'api',
+
+    /**
+     * The base URI for getting search results
+     */
+    'search' => 'search',
+
+    /**
+     * CORS Policy, enabled by default
+     */
+    'cors_policy' => true
+];
