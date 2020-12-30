dc97c8bb3ae0039ca27e0d2e0165b3537e5199b6
George Lemon <things@wemake.co.de>
2020-12-30 13:23:38 +0200

    Adding rainbow js
    Signed-off-by: George Lemon <things@wemake.co.de>

--- themes/twentytwentyone/globals/foot.php
+++ themes/twentytwentyone/globals/foot.php
@@ -1,7 +1,7 @@
 <script src="/assets/js/cookie.js"></script>
 <script src="/assets/js/autoComplete.min.js?v8.2.0"></script>
 <script src="/assets/js/dexie.min.js?v8.2.0"></script>
-<script src="/assets/js/rainbow.js"></script>
+<script src="/assets/js/rainbow.min.js"></script>
 <script type="text/javascript" src="/frontend.js"></script>
 
 </body>
