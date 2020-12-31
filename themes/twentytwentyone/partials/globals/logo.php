09100acee3732b844c03ab1028bc5bc6e0fec70c
George Lemon <things@wemake.co.de>
2020-12-31 21:00:07 +0200

    Make default logo to show the version status of WikiPeep
    Signed-off-by: George Lemon <things@wemake.co.de>

--- themes/twentytwentyone/partials/globals/logo.php
+++ themes/twentytwentyone/partials/globals/logo.php
@@ -3,6 +3,8 @@
         <?php echo sprintf('<a href="/"><img class="img-fluid" alt="'.app_name().'" src="%s"></a>', $appLogo); ?>
     <?php } else { ?>
     <a class="d-block" href="/">
+        <small class="d-block text-uppercase text-left text-dark" style="letter-spacing: 1px; font-family: courier; font-size:8px; margin-top:-10px; font-weight:normal;">
+        Currently in alpha-01</small>
         <svg style="margin-top:4px" viewBox="0 0 24 24" width="28" height="28" stroke="url(#gradient)" stroke-width="1.4" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
             <defs>
                 <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
