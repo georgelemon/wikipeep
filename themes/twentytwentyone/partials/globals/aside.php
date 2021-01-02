adc437d94a3fef21f385ca44dc936adf9e5d4995
George Lemon <georgelemon@protonmail.com>
2021-01-02 18:10:18 +0200

    Replace Rainbow.js with Highlight.js
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- themes/twentytwentyone/partials/globals/aside.php
+++ themes/twentytwentyone/partials/globals/aside.php
@@ -1,20 +1,16 @@
 <div class="main--sidebar col-lg-2" style="z-index:11;">
     <div class="main--sidebar--area p-3">
-        
         <?php echo $this->view('partials/globals/logo') ?>
-
-        <h6 class="text-muted">
-            <small>Summary</small>
-        </h6>
+        <h6 class="text-muted"><small>Summary</small></h6>
         <?php echo getAsideNavigation(); ?>
         <?php
-            if( $box = flywheel()->getAsideBox()) {
+            // if( $box = flywheel()->getAsideBox()) {
                 // echo sprintf('<div class="p-3" style="background-color: %s">%s</div>', $box['_boxColor'], $box['message'], $box['label']);
-            }
+            // }
         ?>
 
         <div class="position-absolute pb-3" style="bottom:0">
-            <small class="text-muted d-block" style="font-size:10px">Made with <a href="#">WikiPeep 1.0.0</a><br>
+            <small class="text-muted d-block" style="font-size:10px">Made with <a href="https://github.com/georgelemon/wikipeep">WikiPeep 1.0.0</a><br>
             An open source wiki for busy devs.</small>
         </div>
     </div>
