83d111b5f4e8d01ed88b609e7d72d2d786b3a249
George Lemon <georgelemon@protonmail.com>
2021-01-06 20:03:58 +0200

    Search bar update
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- themes/twentytwentyone/partials/globals/search-bar.php
+++ themes/twentytwentyone/partials/globals/search-bar.php
@@ -14,7 +14,7 @@
             <input id="searchbar--input" type="text" placeholder="<?php echo config()->get('placeholders.search_bar') ?>" class="form-control">
             <span title="Focus on slash key" class="btn position-absolute btn-sm border">/</span>
         </div>
-        <button id="app--theme--switcher" class="btn float-right">
+        <button id="app--theme--switcher" class="btn float-right" style="margin-right:2%">
             <span class="icon-moon" style="display:none">
                 <svg viewBox="0 0 24 24" width="24" height="24" stroke="black" stroke-width="1" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
             </span>
