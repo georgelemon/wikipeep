baa0129b6f2b8dc13422964d1a3496d63375b572
George Lemon <georgelemon@protonmail.com>
2021-01-15 09:19:36 +0200

    Theme changes
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- themes/twentytwentyone/category.php
+++ themes/twentytwentyone/category.php
@@ -1,42 +1,44 @@
-<?php echo $this->view('partials/globals/aside') ?>
-<div class="col-lg-10 offset-lg-2">
-    <?php echo $this->view('partials/globals/search-bar'); ?>
-    <div class="row">
-        <?php
-            if( $this->contents['summary'] ) {
-                $articleViewColSize = 'col-lg-10 offset-lg-2';
-                echo $this->view('partials/globals/summary');
-            } else {
-                $articleViewColSize = 'col-lg-12';
-            }
-        ?>
-        <div id="article--side" class="<?php echo $articleViewColSize; ?> position-relative">
-            <div class="px-lg-5 pt-lg-4 pb-5">
-                <?php
-                    // When provided in _settings.yaml will get meta heading
-                    // data in order to create a headline and a lead.
-                    echo $this->getCategoryHeading();
-                ?>
-                <div class="category--list--items">
-                <?php foreach ($this->contents as $key => $item): ?>
+<?php
+if( $this->contents['summary'] ) {
+    $colSize = 'col-lg-10 offset-lg-2';
+    $summaryPartialView = $this->view('partials/globals/summary');
+} else {
+    $colSize = 'col-lg-12';
+    $summaryPartialView = '';
+}
 
-                    <div class="mb-3">
-                        <a class="category--item p-4 bg-light rounded d-block text-dark text-decoration-none" href="<?php echo $this->getArticlePermalink($item->getId()); ?>">
-                            <span class="d-block font-weight-bold">
-                                <?php echo $item->title; ?>
-                            </span>
-                            <small class="d-block">Here you can find most common questions related to WikiPeep. If you have anything else that's not covered in here, just </small>
-                        </a>
-                    </div>
-                <?php endforeach; ?>
-                <?php
-                    /**
-                     * When available it renders the pagination at the end of the page
-                     */
-                    echo $this->getPaginationElement()
-                ?>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+echo $this->view('partials/globals/aside');
+
+echo el('div.col-lg-10.offset-lg-2', [
+
+    $this->view('partials/globals/search-bar'),
+    
+    el('div.row', [
+
+        // Rendering summary partial view when available
+        $summaryPartialView,
+        
+        el("div#article--side.position-relative.$colSize > div.px-lg-5.pt-lg-4.pb-5", [
+            // When provided in _settings.yaml will get meta heading
+            // data in order to create a headline and a lead.
+            $this->getCategoryHeading(),
+
+            el('div.category--list--items',
+
+                // Looping through the  available  articles using loop() handler
+                loop($this->contents, function($item) {
+
+                    $hyperlink = $this->getArticlePermalink($item->getId());
+
+                    return el('div.mb-3', [
+                        el("a.category--item.p-4.bg-light.rounded.d-block.text-dark.text-decoration-none[href='$hyperlink']", [
+                                el('span.d-block.font-weight-bold', $item->title),
+                                el('small.d-block', "Here you can find most common questions related to WikiPeep. If you have anything else that's not covered in here, just ")
+                            ]
+                        )
+                    ]);
+                }) . $this->getPaginationElement()
+            )
+        ])
+    ])
+]);
--- themes/twentytwentyone/partials/globals/aside.php
+++ themes/twentytwentyone/partials/globals/aside.php
@@ -1,17 +1,22 @@
-<div class="main--sidebar col-lg-2" style="z-index:11;">
-    <div class="main--sidebar--area p-3">
-        <?php echo $this->view('partials/globals/logo') ?>
-        <h6 class="text-muted"><small>Summary</small></h6>
-        <?php echo getAsideNavigation(); ?>
-        <?php
-            // if( $box = flywheel()->getAsideBox()) {
-                // echo sprintf('<div class="p-3" style="background-color: %s">%s</div>', $box['_boxColor'], $box['message'], $box['label']);
-            // }
-        ?>
+<?php
 
-        <div class="wikipeep-copyright-aside position-absolute pb-3 ml-4" style="bottom:0; left:0">
-            <small class="p-2 d-block rounded" style="border: 1px #addedd dotted; font-size:10px">Made with <a href="https://github.com/georgelemon/wikipeep">WikiPeep 1.0.0</a><br>
-            An open source Wiki for busy devs</small>
-        </div>
-    </div>
-</div>
+echo el('div.main--sidebar.col-lg-2', ['style' => 'z-index:11'], [
+    
+    el('div.main--sidebar--area.p-3', [
+        
+        // Rendering the current logo
+        $this->view('partials/globals/logo'),
+
+        // Render heading of the aside 
+        el('h6.text-muted > small', 'Summary'),
+
+        // Render the main navigation bar
+        getAsideNavigation(),
+
+        el('div.wikipeep-copyright-aside.position-absolute.pb-3.ml-4', ['style' => 'bottom:0; left:0;'],
+            [
+                el('small.p-2.d-block.rounded', ['style' => 'border:1px #addedd dotted; font-size:10px;'], 'Made with WikiPeep v' . APP_VERSION . '<br> Open Source wiki for Busy Devs')
+            ]
+        )
+    ])
+]);
