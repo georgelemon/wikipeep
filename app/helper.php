52bbdbcbe9f7e89c8d32639aeb899839e4c1e33a
George Lemon <georgelemon@protonmail.com>
2021-04-29 19:24:50 +0300

    Add function helper to retrieve current app settings
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- app/helper.php
+++ app/helper.php
@@ -74,11 +74,67 @@ function get_formatted_date($strDate, $fromFormat, $toFormat, string $timezone =
 }
 
 /**
- * Get a basic, readable formatted date 
+ * Get a basic, readable formatted date.
+ * 
  * @return string
  */
 function get_date($strDate)
 {
     $date = new DateTime($strDate);
     return $date->format(config()->get('app.date_format'));
+}
+
+
+/**
+ *
+ * It injects an inline JavaScript array in the head of your WikiPeep,
+ * in order to create a simple bridge that will be used via JavaScript
+ * by search related functionality.
+ * 
+ * @return string
+ */
+function getApplicationSettings()
+{
+    $api = config()->get('api');
+    $vers = glob(STORAGE_PATH . '/__search--version--*');
+    if( $vers ) {
+        $vers = $vers[0];
+        $vers = explode(STORAGE_PATH , $vers)[1];
+        if( str_contains($vers, '/') ) {
+            $vers = str_replace('/__search--version--', '', $vers);
+        } else {
+            $vers = str_replace('__search--version--', '', $vers);
+        }
+    } else {
+        $vers = 10; // default version
+    }
+
+
+    // Try get the version of the search results from disk
+    // if( $filesystem->exists(STORAGE_PATH . '') ) {
+    //     // $version = 
+    // }
+
+    return sprintf('<script>const __settings = %s</script>', json_encode([
+                // The URL of the app
+                'app_url' => uri()->base(),
+                // // Whether to serve the app over https protocol
+                // 'app_ssl' => env('SSL'),
+                // The base API endpoint.
+                'api_base' => $api['base'],
+                // Search Results API Endpoint is used by the search
+                // to fetch data via JavaScript. For changing the default endpoint
+                // go to configs/api.php
+                'search_endpoint' => $api['search'],
+                // The latest search update is used for keeping the track
+                // of builds so people who visited your WikiPeep before
+                // can get the latest version of search results directly to their IndexedDB.
+                // Gets filled with the date of the last build.
+                'latest_search_update' => $vers,
+                // The only thing related to cookies that WikiPeep
+                // does by default is to set a cookie for theme preference.
+                'cookie_settings' => getCookieSettings(),
+                // Retrieve the cookie disclaimer message
+                'cookie_disclaimer' => getCookeDisclaimer()
+        ]));
 }
