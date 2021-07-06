af182a20652020717f3e2ff9ef9ffaa116f5ddc6
George Lemon <georgelemon@protonmail.com>
2021-07-06 19:01:31 +0300

    Frontend setup
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

---
+++ frontend/rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+    input: 'src/app.js',
+    output: {
+        name: 'Wikipeep',
+        file: `../public/assets/app.min.js`,
+        format: 'iife',                       // "amd", "cjs", "system", "es", "iife" or "umd"
+        sourcemap: true
+    },
+    plugins: [
+        resolve(),
+        commonjs({
+            transformMixedEsModules: true,
+        }),
+        // terser({
+        //     format: {
+        //       comments: false
+        //     },
+        // }),
+    ]
+};
